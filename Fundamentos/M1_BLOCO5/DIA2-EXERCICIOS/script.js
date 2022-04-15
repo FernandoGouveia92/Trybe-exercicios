71aa6a32e91ad9f76d64e8a6bc957dafd4371fa5
Fernando Gouveia <fernando_gouveia2@hotmail.com>
2022-04-14 21:55:39 -0300

    exercicios feitos, ate o 8

---
+++ Fundamentos/M1_BLOCO5/DIA2-EXERCICIOS/script.js
@@ -0,0 +1,4 @@
+//document.getElementById('elementoOndeVoceEsta').parentElement.style.color = 'green';
+
+
+console.log(document.getElementById('pai').parentElement.style.color = 'blue');
--- Fundamentos/M1_BLOCO5/dia-2-javascript-trabalhando-com-elementos/script.js
+++ Fundamentos/M1_BLOCO5/dia-2-javascript-trabalhando-com-elementos/script.js
@@ -1,3 +1,13 @@
 //document.getElementById('elementoOndeVoceEsta').parentElement.style.color = 'green';
 
-console.log(document.getElementById('pai').parentElement.style.color = 'blue');
+// console.log(document.getElementById('pai').parentElement.style.color = 'blue');
+
+let container = document.getElementsByTagName('body');
+// Eu criei uma vairável com a TAG BODY selecionada, para futuro uso(em uma função por exemplo)
+let newH1 = document.createElement('h1');
+// inserindo texto na variavel nova (tag nova)
+newH1.innerText = 'Exercício 5.2 - JavaScript DOM';
+//inserindo no
+container.appendChild(newH1);
+
+
