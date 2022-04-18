8922498280fb5d0d6586ebae4d85c0e6b60845dd
Fernando Gouveia <fernando_gouveia2@hotmail.com>
2022-04-18 15:20:46 -0300

    exercicios

--- Fundamentos/M1_BLOCO5/dia-2-javascript-trabalhando-com-elementos/script.js
+++ Fundamentos/M1_BLOCO5/dia-2-javascript-trabalhando-com-elementos/script.js
@@ -2,12 +2,12 @@
 
 // console.log(document.getElementById('pai').parentElement.style.color = 'blue');
 
-let container = document.getElementsByTagName('body');
-// Eu criei uma vairável com a TAG BODY selecionada, para futuro uso(em uma função por exemplo)
-let newH1 = document.createElement('h1');
-// inserindo texto na variavel nova (tag nova)
-newH1.innerText = 'Exercício 5.2 - JavaScript DOM';
-//inserindo no
-container.appendChild(newH1);
+// let container = document.getElementsByTagName('body');
+// // Eu criei uma vairável com a TAG BODY selecionada, para futuro uso(em uma função por exemplo)
+// let newH1 = document.createElement('h1');
+// // inserindo texto na variavel nova (tag nova)
+// newH1.innerText = 'Exercício 5.2 - JavaScript DOM';
+// //inserindo no
+// container.appendChild(newH1);
 
 
