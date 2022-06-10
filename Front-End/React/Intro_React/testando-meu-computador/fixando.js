a25ced4a5ead80cff65732afadf0d81723e746a2
Fernando Gouveia <fernando_gouveia2@hotmail.com>
2022-06-10 16:34:33 -0300

    ajustando git

--- Front-End/React/Intro_React/testando-meu-computador/fixando.js
+++
@@ -1,6 +0,0 @@
-// const classe = 'bom-humor';
-// const textJSX = 'JSX';
-// const saudacaoReact = <h1 className={classe}>Hello, {textJSX}!!</h1>;
-// const container = <div>{saudacaoReact}</div>
-
-// console.log(container);
