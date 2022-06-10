9fd8ea41c207a69827650e1b0d78a7f1b701f9e8
Fernando Gouveia <fernando_gouveia2@hotmail.com>
2022-06-10 16:02:19 -0300

    .

---
+++ Front-End/React/Intro_React/testando-meu-computador/fixando.js
@@ -0,0 +1,6 @@
+// const classe = 'bom-humor';
+// const textJSX = 'JSX';
+// const saudacaoReact = <h1 className={classe}>Hello, {textJSX}!!</h1>;
+// const container = <div>{saudacaoReact}</div>
+
+// console.log(container);
