e38b5e4ed20be2ca193e51423fba98e452b2bbed
Fernando Gouveia <fernando_gouveia2@hotmail.com>
2022-06-23 15:41:12 -0300

    adc conteudoa

---
+++ Front-End/React/Proj_Paralelo_React/Proj_Weather_App/openWeatherApp/src/App.jsx
@@ -0,0 +1,79 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+import './App.css'
+// import header from './header'
+
+function App() {
+  const [data, setData] = useState({})
+  const [location, setLocation] = useState('')
+  
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=ff71599b9e11ad701438eb58a8ba36cd`
+
+  const searchLocation = (event) => {
+    if (event.key === 'Enter') {
+
+      axios.get(url).then((response) => { // biblioteca que trata o dado da fetch API direto - AXIOS
+        setData(response.data)
+      })
+      
+      setLocation('')
+    }
+  }
+
+  return (
+    <div className="app">
+      {/* </ header> */}
+      <div className="search">
+        <input
+        value= {location}
+        onChange={event => setLocation(event.target.value)}
+        onKeyPress={searchLocation}
+        placeholder="digite a cidade desejada"
+        type="text" />
+      </div>
+
+      <div className="container">
+
+        <div className="top">
+
+          <div className="location">
+            <p>{data.name}</p>
+          </div>
+
+          <div className="temp">
+            {data.main ? <p>{data.main.temp}ºC</p> : null }
+          </div>
+
+          <div className="description">
+            {data.weather ? <p>{data.weather[0].main}</p> : null }
+          </div>
+        </div>
+
+        {data.name !== undefined &&
+
+        <div className="bottom">
+
+          <div className="feels">
+            {data.main ? <p className='bold'>{data.main.feels_like}ºC</p> : null}
+            <p>Feels like</p>
+          </div>
+
+          <div className="humidity">
+            {data.main ? <p className='bold'>{data.main.humidity}%</p> : null}
+            <p>Humidity</p>
+          </div>
+
+          <div className="wind">
+            {data.wind ? <p className='bold'>{data.wind.speed}</p> : null}
+            <p>Wind</p> 
+          </div>
+        </div>
+        }
+
+      </div>
+
+    </div>
+  )
+}
+
+export default App
